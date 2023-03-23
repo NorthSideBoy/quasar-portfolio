cb7558983748c2fb2d74e470c865f2327fa7e8c4
Said Lucena <saidlucena519@gmail.com>
2023-03-23 00:14:31 -0400

    animations

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -12,7 +12,8 @@
           &lt;h1&gt;
         </span>
       </div>
-      <div style="padding-left: 5vw;">
+      <div style="padding-left: 5vw;" class="animate__animated animate__fadeInLeft"
+        @animationend="(e) => displaySubtitle(e, 'animate__fadeInLeft')">
         <h1 style="font-family: 'fira-bold'; font-size: calc(30px + 3vw);" class="text-white no-margin no-padding;">
           <span style="letter-spacing: -0.2rem;">
             Hola, <br>
@@ -33,10 +34,17 @@
           &lt;p&gt;
         </span>
       </div>
-      <div style="padding-left: 5vw;">
+      <div style="padding-left: 5vw;" v-if="subtitle" class="animate__animated animate__fadeIn">
         <p style="font-family: 'fira-bold'; font-size: calc(2px + 2vw);" class="text-grey no-margin no-padding;">
           <span>
             Back End - Front End Developer
+          </span>
+        </p>
+      </div>
+      <div v-else>
+        <p style="font-family: 'fira-bold'; font-size: calc(2px + 2vw);" class="no-margin no-padding;">
+          <span>
+            &nbsp;
           </span>
         </p>
       </div>
@@ -46,15 +54,18 @@
         </span>
       </div>
     </div>
-    <div style="padding-left: 5vw;" class="q-pt-lg q-pb-lg">
-      <q-btn class="q-pl-lg q-pr-lg" flat label="CONTACTAME"
+    <div style="padding-left: 5vw;" class="q-pt-lg q-pb-lg" >
+      <q-btn class="q-pl-lg q-pr-lg animate__animated animate__headShake animate__delay-1s" flat label="CONTACTAME" v-if="contact"
         style="font-family: 'fira-light'; color: #1D8EF0; border: 1px solid #1D8EF0; border-radius: 0px 0px 0px 0px;"
         size="lg">
-
+      </q-btn>
+      <q-btn class="q-pl-lg q-pr-lg" flat label="CONTACTAME" v-else
+        style="font-family: 'fira-light'; color: #1D8EF0; border: 1px solid #1D8EF0; border-radius: 0px 0px 0px 0px;"
+        size="lg">
       </q-btn>
     </div>
     <div style="font-family: 'fira-medium';" class="q-pl-lg">
-      <span style="color: #A5D6F0">
+      <span style="color: #A5D6F0;">
         &lt;/section&gt;
       </span>
     </div>
@@ -65,7 +76,18 @@ import { ref } from 'vue'
 import Particles from 'src/components/Particles.vue';
 export default {
   setup() {
+    const contact = ref(false)
+    const subtitle = ref(false)
+    const displaySubtitle = (e, animation) => {
+      const prefix = "animated__"
+      e.target.classList.remove(`${prefix}animated`, animation);
+      subtitle.value = !subtitle.value
+      contact.value = true
+    }
     return {
+      contact,
+      subtitle,
+      displaySubtitle
     }
   },
   components: {
